6ef4b46b970b24ab27255b1b6bf6e0aff9c59ad4
Twig Nielsen <littleb88888888@gmail.com>
2025-04-05 12:15:00 -0600

    Implemented mannequin "walk" - split test animation script into functions - moved the node on each "step" - randomized movement parameters

--- scripts/mannequin.gd
+++ scripts/mannequin.gd
@@ -1,29 +1,47 @@
 extends Skeleton3D
 
 @export var angle: float = PI/12
-@export var randomize_delay: float = 1
-@export var step_offset: float = 0.2
+@export var min_step_delay: float = 0.05
+@export var max_step_delay: float = 0.15
+
+@export var min_step_distance := Vector2(0.1, 0.5)
+@export var max_step_distance := Vector2(0.3, 1)
 
 var step_direction: int = 1
 
 
 func _ready() -> void:
-    var tween = create_tween();
-    tween.set_loops()
-    tween.tween_callback(randomize_pose)
-    tween.tween_interval(randomize_delay)
+    take_step()
 
 
-func randomize_pose():
+func take_step():
+    move_with_step()
+    set_base_position()
+    randomize_pose()
+    var tween = create_tween()
+    tween.tween_interval(randf_range(min_step_delay, max_step_delay))
+    tween.tween_callback(take_step)
+
+
+func move_with_step():
+    position.z += randf_range(min_step_distance.y, max_step_distance.y)
+    if position.z > 0:
+        position.z -= 10
+
+
+func set_base_position():
     step_direction *= -1
     var base_position := get_bone_rest(0).origin
-    base_position.x += step_direction * step_offset
+    base_position.x += step_direction * randf_range(min_step_distance.x, max_step_distance.x)
     var base_scale = Vector3(-step_direction, 1, 1)
 
     var base_t = Transform3D(
         Basis.from_scale(base_scale), base_position
     )
     set_bone_pose(0, base_t)
+
+
+func randomize_pose():
 
     for i in range(1, get_bone_count()):
         if get_bone_name(i) == "Right Foot": continue
