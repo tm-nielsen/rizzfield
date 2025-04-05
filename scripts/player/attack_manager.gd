55e996e9564ef97f7acb6270da5d963df2913069
Twig Nielsen <littleb88888888@gmail.com>
2025-04-04 22:30:08 -0600

    Optimized attack feel - cut anticipation frame from charged swing - added swing anticipation frame as separate animation that plays before an uncharged swing - refined attack manager script

--- scripts/player/attack_manager.gd
+++ scripts/player/attack_manager.gd
@@ -1,20 +1,26 @@
 extends Node3D
 
-enum AttackState {IDLE, CHARGING, CHARGED, SWINGING}
+enum AttackState {
+    IDLE, CHARGING, CHARGED,
+    STARTING_WEAK_SWING, SWINGING
+}
 var state: AttackState
 
 @export var animator: AnimationPlayer
+
+var attack_pressed: bool;
 
 
 func _ready() -> void:
     animator.animation_finished.connect(_on_animation_finished)
 
 func _process(_delta: float) -> void:
-    if Input.is_action_pressed("attack"):
-        if state == AttackState.IDLE: set_state(AttackState.CHARGING)
-    else: match state:
-        # AttackState.CHARGING: set_state(AttackState.IDLE)
-        AttackState.CHARGED: set_state(AttackState.SWINGING)
+    attack_pressed = Input.is_action_pressed("attack")
+    match state:
+        AttackState.IDLE:
+            if attack_pressed: set_state(AttackState.CHARGING)
+        AttackState.CHARGED:
+            if !attack_pressed: set_state(AttackState.SWINGING)
 
 
 func set_state(new_state: AttackState):
@@ -22,11 +28,15 @@ func set_state(new_state: AttackState):
         AttackState.IDLE: animator.play("idle")
         AttackState.CHARGING: animator.play("charge")
         AttackState.CHARGED: animator.play("hold_charge")
+        AttackState.STARTING_WEAK_SWING: animator.play("swing_windup")
         AttackState.SWINGING: animator.play("swing")
     state = new_state
 
 
-func _on_animation_finished(animation_name: String):
-    match animation_name:
-        "charge": set_state(AttackState.CHARGED)
-        "swing": set_state(AttackState.IDLE)
+func _on_animation_finished(_animation_name: String):
+    match state:
+        AttackState.CHARGING: 
+            if (attack_pressed): set_state(AttackState.CHARGED)
+            else: set_state(AttackState.STARTING_WEAK_SWING)
+        AttackState.STARTING_WEAK_SWING: set_state(AttackState.SWINGING)
+        AttackState.SWINGING: set_state(AttackState.IDLE)
