bfe0ada6531b8abaf2a17dae07178dee26b0036f
Twig Nielsen <littleb88888888@gmail.com>
2025-04-12 19:50:57 -0600

    Added basic looking

--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -2,9 +2,18 @@ extends CharacterBody3D
 
 @export var camera: Camera3D
 @export var movement_speed: float = 10.0
+@export var look_speed := Vector2(0.04, 0.02)
+@export var minimum_look_angle: float = -PI/8
+@export var maximum_look_angle: float = PI/8
 
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
+    move()
+    look(delta)
+    move_and_slide()
+
+
+func move():
     var input_direction = Input.get_vector(
         "left", "right", "forward", "backward"
     )
@@ -14,4 +23,12 @@ func _physics_process(_delta: float) -> void:
     velocity.z = planar_velocity.y
     velocity += get_gravity()
 
-    move_and_slide()
+
+func look(delta: float):
+    var mouse_delta = Input.get_last_mouse_velocity()
+    var look_delta = mouse_delta * look_speed * delta
+    transform = transform.rotated_local(Vector3.UP, -look_delta.x)
+    camera.rotation.x = clampf(
+        camera.rotation.x - look_delta.y,
+        minimum_look_angle, maximum_look_angle
+    )
