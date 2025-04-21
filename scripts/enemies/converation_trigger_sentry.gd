c403bb435df5913a01f54249d2b67fd3dd95483d
Twig Nielsen <littleb88888888@gmail.com>
2025-04-21 14:54:15 -0600

    Fixed conversation trigger - swapped out raycast node for a manual raycast - added angle check - waited to spawn combat instance unless combat is triggered - delayed tree pause until next frame to fix camera update - made conversation trigger script inherit from character body and move and slide to stick to ground - added skin material to conversation trigger mannequin

--- scripts/enemies/converation_trigger_sentry.gd
+++ scripts/enemies/converation_trigger_sentry.gd
@@ -1,15 +1,13 @@
-extends Node3D
+extends CharacterBody3D
 
 @export var vignette_prefab: PackedScene
 @export var combat_prefab: PackedScene
-
-@export_subgroup("node references")
 @export var head_node: BoneAttachment3D
-@export var raycast: RayCast3D
 
 @export_subgroup("parameters")
-@export_range(0, 180) var view_range = 60
+@export_range(0, 180) var view_range: float = 60
 @export var activation_time: float = 1 / 12.0
+@export_flags_3d_physics var raycast_mask = 3
 
 var camera: Camera3D
 var triggered: bool = false
@@ -20,11 +18,27 @@ func _ready() -> void:
     head_node.override_pose = false;
     camera = get_viewport().get_camera_3d()
 
-func _process(_delta: float) -> void:
-    if raycast.is_colliding():
+func _physics_process(_delta: float) -> void:
+    if check_angle() && check_raycast():
         activate()
-    else:
-        raycast.target_position = camera.global_position - raycast.global_position
+
+    velocity += get_gravity()
+    move_and_slide()
+
+
+func check_angle() -> bool:
+    var diff = camera.global_position - head_node.global_position
+    return diff.angle_to(-head_node.basis.z) < deg_to_rad(view_range)
+
+func check_raycast() -> bool:
+    var space_state := get_world_3d().direct_space_state
+    var result := space_state.intersect_ray(
+        PhysicsRayQueryParameters3D.create(
+        head_node.global_position,
+        camera.global_position,
+        raycast_mask)
+    )
+    return result != {} && result.collider is PlayerController
 
 
 func activate():
@@ -34,14 +48,17 @@ func activate():
     head_node.look_at(camera.global_position)
     camera.look_at(head_node.global_position)
 
-    get_tree().paused = true;
+    (func(): get_tree().paused = true).call_deferred()
     TweenHelpers.call_delayed_realtime(start_conversation, activation_time)
 
 func start_conversation():
     var vignette_instance: Node3D = vignette_prefab.instantiate()
     vignette_instance.global_transform = global_transform
+    GameModeSignalBus.combat_triggered.connect(spawn_combat_instance)
     GameModeSignalBus.notify_conversation_triggered(vignette_instance)
 
+func spawn_combat_instance():
     var combat_instance: Node3D = combat_prefab.instantiate()
     add_sibling(combat_instance)
     combat_instance.transform = transform
+    queue_free()
