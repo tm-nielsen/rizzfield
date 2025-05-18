9ddac523e84370e85a7cf508845d1f41914c5cc3
Twig Nielsen <littleb88888888@gmail.com>
2025-05-18 15:18:57 -0600

    Frame limited fragment pickup animation

--- scripts/exploration/fragment_pickup_area.gd
+++ scripts/exploration/fragment_pickup_area.gd
@@ -7,6 +7,7 @@ extends Area3D
 @export var spin_speed: float = 2
 @export var bob_frequency: float = 2
 @export var bob_amplitude: float = 0.15
+@export var frame_timer: FrameTimer
 
 var mesh: MeshInstance3D
 
@@ -17,15 +18,16 @@ func _ready() -> void:
         ExplorationSignalBus.notify_fragment_collected(fragment)
         queue_free()
     )
+    frame_timer.frame_out.connect(update_mesh_rotation)
     mesh = fragment.create_mesh_instance()
     mesh.material_override = fragment.create_material()
     add_child(mesh)
     mesh.scale = Vector3.ONE * mesh_scale
 
 
-func _process(delta: float) -> void:
-    if !mesh: return;
-    mesh.rotate_y(spin_speed * delta)
+func update_mesh_rotation() -> void:
+    if !mesh: return
+    mesh.rotate_y(spin_speed * frame_timer.frame_period)
     mesh.position.y = sin(
         bob_frequency * Time.get_ticks_msec() / 1000
     ) * bob_amplitude
