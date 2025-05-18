5d60b96d79f1ac09869b83a907b1450107c2bfbd
Twig Nielsen <littleb88888888@gmail.com>
2025-05-18 11:34:19 -0600

    Animated fragment pickup - added simple rotation and bob animation to fragment pickup - programmatically set mesh from referenced fragment

--- scripts/exploration/fragment_pickup_area.gd
+++ scripts/exploration/fragment_pickup_area.gd
@@ -2,6 +2,13 @@ extends Area3D
 
 @export var fragment: ResponseFragment
 
+@export_subgroup("animation")
+@export var spin_speed: float = 2
+@export var bob_frequency: float = 2
+@export var bob_amplitude: float = 0.15
+
+var mesh: MeshInstance3D
+
 
 func _ready() -> void:
     body_entered.connect(func(body: PhysicsBody3D):
@@ -9,3 +16,15 @@ func _ready() -> void:
         ExplorationSignalBus.notify_fragment_collected(fragment)
         queue_free()
     )
+    for child in get_children():
+        if child is MeshInstance3D:
+            mesh = child
+            mesh.mesh = fragment.mesh
+
+
+func _process(delta: float) -> void:
+    if !mesh: return;
+    mesh.rotate_y(spin_speed * delta)
+    mesh.position.y = sin(
+        bob_frequency * Time.get_ticks_msec() / 1000
+    ) * bob_amplitude
