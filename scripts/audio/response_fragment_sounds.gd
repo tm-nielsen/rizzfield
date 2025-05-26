564ae4063b7c629289c440e888c69cd7b7ffa856
Twig Nielsen <littleb88888888@gmail.com>
2025-05-26 14:08:31 -0600

    Fixed fragment rollover sound - prevented sound from replaying if the fragment is held *(dragging it around)*

--- scripts/audio/response_fragment_sounds.gd
+++ scripts/audio/response_fragment_sounds.gd
@@ -6,6 +6,10 @@ extends AudioClipPlayer
 
 func _ready() -> void:
     var fragment_body: ResponseFragmentBody = get_parent()
-    connect_clip(fragment_body.mouse_entered, mouseover)
+    fragment_body.mouse_entered.connect(
+        func():
+        if fragment_body.state != fragment_body.HELD:
+            play_clip(mouseover)
+    )
     connect_clip(fragment_body.grabbed, grab)
     connect_clip(fragment_body.dropped, drop)
