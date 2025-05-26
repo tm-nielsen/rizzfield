8fced32d092523a0f3a0f3087579e9e7792d6c86
Twig Nielsen <littleb88888888@gmail.com>
2025-05-26 10:32:22 -0600

    Reworked random audio player to leverage built-in type

--- scripts/audio/layered_random_sound_player.gd
+++ scripts/audio/layered_random_sound_player.gd
@@ -2,22 +2,16 @@ class_name LayeredRandomSoundPlayer
 extends Node3D
 
 @export_range(0, 1) var volume: float = 1
-@export var layer_clip_sets: Array[RandomSoundLayerClipSet]
+@export var layer_streams: Array[AudioStreamRandomizer]
 
 var layer_sources: Array[AudioStreamPlayer3D]
-var layer_count: int
-
 
 func _ready() -> void:
-    layer_count = layer_clip_sets.size()
-    for layer in layer_clip_sets:
+    for layer in layer_streams:
         var new_source = AudioStreamPlayer3D.new()
-        new_source.volume_linear = volume * layer.volume
+        new_source.volume_linear = volume
         new_source.bus = "Sounds"
+        new_source.stream = layer
         add_child(new_source)
 
-func play():
-    for i in layer_count:
-        var source = layer_sources[i]
-        source.clip = layer_clip_sets[i].pick()
-        source.play()
+func play(): for source in layer_sources: source.play()
--- scripts/audio/random_sound_layer_clip_set.gd
+++
@@ -1,8 +0,0 @@
-class_name RandomSoundLayerClipSet
-extends Resource
-
-@export_range(0, 1) var volume: float = 1
-@export var clips: Array[AudioStream]
-
-func pick() -> AudioStream:
-    return clips.pick_random()
